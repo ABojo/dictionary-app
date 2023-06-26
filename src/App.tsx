49ee86c6bdae0a7aa05ef6cebaf000106bc66a5a
ABojo <Alex.bojorquez2598@gmail.com>
2023-06-25 22:23:51 -0400

    built search bar component that will hold its own input/error state + take an onsubmit handler and feed the input value into it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Container from "./components/Container/Container";
 import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <div>
       <Container>
         <Header />
+        <SearchBar
+          onSubmit={(keyword: string) => {
+            //
+          }}
+        />
       </Container>
     </div>
   );
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,44 @@
+import styles from "./search-bar.module.scss";
+import { useState } from "react";
+
+interface SearchBarProps {
+  onSubmit: (keyword: string) => void;
+}
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const [inputValue, setInputValue] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  function submitHandler(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!inputValue) {
+      return setIsInvalid(true);
+    }
+
+    setIsInvalid(false);
+    onSubmit(inputValue);
+  }
+
+  return (
+    <form
+      className={`${styles["search-form"]} ${isInvalid ? styles["search-form--invalid"] : ""}`}
+      onSubmit={submitHandler}
+    >
+      <div className={styles["search-form__input-container"]}>
+        <input
+          className={styles["search-form__input"]}
+          placeholder="Search for any word..."
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        ></input>
+        <button className={styles["search-form__button"]} type="submit">
+          <img src="images/icon-search.svg" alt="search icon" />
+        </button>
+      </div>
+      <span className={styles["search-form__error-message"]}>Whoops, can't be empty...</span>
+    </form>
+  );
+}
