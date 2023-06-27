5e5cd5035a45092d53f0cff25a2493dfe7c8b0bd
ABojo <Alex.bojorquez2598@gmail.com>
2023-06-26 20:37:31 -0400

    created a function for fetching word data to be used with react query. I also created types for the API response objects

---
+++ src/utils/getWord.ts
@@ -0,0 +1,53 @@
+import { QueryFunctionContext } from "react-query";
+
+interface License {
+  name: string;
+  url: string;
+}
+
+interface Phonetic {
+  text?: string;
+  audio: string;
+  sourceUrl?: string;
+  license?: License;
+}
+
+interface Definition {
+  definition: string;
+  synonyms: string[];
+  antonyms: string[];
+}
+
+interface Meaning {
+  partOfSpeech: string;
+  definitions?: Definition[];
+  synonyms: string[];
+  antonyms: string[];
+}
+
+interface ErrorData {
+  title: string;
+  message: string;
+  resolution: string;
+}
+
+export interface WordData {
+  word: string;
+  phonetic?: string;
+  phonetics: Phonetic[];
+  meanings: Meaning[];
+  license: License;
+  sourceUrls: string[];
+}
+
+export async function getWord({ queryKey }: QueryFunctionContext<string[]>): Promise<WordData> {
+  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${queryKey[1]}`);
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw json;
+  }
+
+  return json[0] as WordData;
+}
