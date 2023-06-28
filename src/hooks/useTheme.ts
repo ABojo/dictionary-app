e30ab4fb5f9021c040a4cdf15d7dd7908d1e6849
ABojo <Alex.bojorquez2598@gmail.com>
2023-06-28 18:34:51 -0400

    added a fonts map that will be used to associate the font classes with a formatted string describing this. This map will be used to help construct the dropdown font switcher

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,35 +3,42 @@ import storage from "../utils/storage";
 
 interface Theme {
   darkMode: boolean;
-  fontFamily: string;
+  fontClass: string;
 }
 
 //inital theme state being built with stored data/fallbacks
 const initialTheme: Theme = {
   darkMode: storage.getDarkMode(),
-  fontFamily: storage.getFontFamily() || "sans",
+  fontClass: storage.getFontFamily() || "sans",
 };
 
 //hook to update state, local storage, and run a side effect to update the theme classes
 export default function useTheme() {
   const [theme, setTheme] = useState(initialTheme);
-  const { darkMode, fontFamily } = theme;
+  const { darkMode, fontClass } = theme;
+
+  //map of font classes and their formatted names
+  const fonts: { [k: string]: string } = {
+    sans: "Sans Serif",
+    serif: "Serif",
+    mono: "Mono",
+  };
 
   //wipe and rebuild the classes attached to the root element when theme state changes
   useEffect(() => {
     document.documentElement.className = "";
-    document.documentElement.className = `${darkMode && "dark"} ${fontFamily}`;
-  }, [darkMode, fontFamily]);
+    document.documentElement.className = `${darkMode && "dark"} ${fontClass}`;
+  }, [darkMode, fontClass]);
 
   function setDarkMode(isEnabled: boolean) {
     setTheme({ ...theme, darkMode: isEnabled });
     storage.setDarkMode(isEnabled);
   }
 
-  function setFontFamily(fontFamily: string) {
-    setTheme({ ...theme, fontFamily });
-    storage.setFontFamily(fontFamily);
+  function setFontClass(fontClass: string) {
+    setTheme({ ...theme, fontClass });
+    storage.setFontFamily(fontClass);
   }
 
-  return { theme, setDarkMode, setFontFamily };
+  return { theme, fonts, setDarkMode, setFontClass };
 }
